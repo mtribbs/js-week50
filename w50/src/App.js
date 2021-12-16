5e032ee2739c24b05da83d147150ad56d96223de
mtribbs <89516065+mtribbs@users.noreply.github.com>
2021-12-16 11:12:31 +0100

    router

--- w50/src/App.js
+++ w50/src/App.js
@@ -1,27 +1,25 @@
-import { useState } from "react";
-import Tag from "./components/Tag";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+
 import "./index.css";
 
-const initialNames = [
-  {firstName: "Dourboum", lastName: "Bloodgranite"},
-  {firstName: "Alforhomli", lastName: "Bitterhide"},
-  {firstName: "Rugnak", lastName: "Ambershoulder"},
-  {firstName: "Modgreat", lastName: "Graygut"},
-  {firstName: "Yubroc", lastName: "Woldmace"},
-]
+
 
 function App() {
-  const [names, setNames] = useState(initialNames);
-
   return (
-  <div className="container">
-    <h1>List of names</h1>
-    {names.map((v, i) => {
-      return (
-        <Tag key={i} firstName={v.firstName} lastName={v.lastName}></Tag>
-      )
-    })}
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </div>
+  </BrowserRouter>
   );
 }
 
